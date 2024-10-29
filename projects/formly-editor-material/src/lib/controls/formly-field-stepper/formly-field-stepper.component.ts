b6bbb720520c0a2304436e356cb73a6acae53bb1
timcyspet <timcyspet@live.com>
2024-10-29 10:45:13 +0000

    Update formly-field-stepper.component.ts

--- projects/formly-editor-material/src/lib/controls/formly-field-stepper/formly-field-stepper.component.ts
+++ projects/formly-editor-material/src/lib/controls/formly-field-stepper/formly-field-stepper.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FieldArrayType,FieldType, FormlyModule ,FormlyFieldConfig } from '@ngx-formly/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
-  selector: 'lib-formly-field-stepper',
+  selector: 'formly-field-stepper',
   standalone: true,
-  imports: [],
+  imports: [FormlyModule, MatIconButton, MatIcon, MatButton,MatStepperModule],
   templateUrl: './formly-field-stepper.component.html',
-  styleUrl: './formly-field-stepper.component.css'
+  styleUrl: './formly-field-stepper.component.scss'
 })
-export class FormlyFieldStepperComponent {
-
+export class FormlyFieldStepper extends FieldType {
+    isValid(field: FormlyFieldConfig): boolean {        
+      if (field.key) {
+        return field.formControl.valid;
+      }  
+      return field.fieldGroup ? field.fieldGroup.every((f) => this.isValid(f)) : true;
+    }
 }

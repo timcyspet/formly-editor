60b173f63420563612d27eb80075a59742a235ed
timcyspet <timcyspet@live.com>
2024-10-29 11:01:12 +0000

    Update formly-field-tabs.component.ts

--- projects/formly-editor-material/src/lib/controls/formly-field-tabs/formly-field-tabs.component.ts
+++ projects/formly-editor-material/src/lib/controls/formly-field-tabs/formly-field-tabs.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FieldArrayType,FieldType, FormlyModule ,FormlyFieldConfig } from '@ngx-formly/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
-  selector: 'lib-formly-field-tabs',
+  selector: 'formly-field-tabs',
   standalone: true,
-  imports: [],
+  imports:  [FormlyModule, MatIconButton, MatIcon, MatButton,MatTabsModule],
   templateUrl: './formly-field-tabs.component.html',
-  styleUrl: './formly-field-tabs.component.css'
+  styleUrl: './formly-field-tabs.component.scss'
 })
-export class FormlyFieldTabsComponent {
-
+export class FormlyFieldTabs extends FieldType {
+    isValid(field: FormlyFieldConfig): boolean {
+      if (field.key) {
+        return field.formControl.valid;
+      }
+  
+      return field.fieldGroup ? field.fieldGroup.every((f) => this.isValid(f)) : true;
+    }
 }
